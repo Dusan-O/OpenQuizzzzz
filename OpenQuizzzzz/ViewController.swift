551386f49fc82cfb07e9bfa0a8b02fca664c099e
Dusan-O <dusan.orescanin@gmail.com>
2022-01-25 02:38:39 +0100

    Initial Commit

---
+++ OpenQuizzzzz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenQuizzzzz
+//
+//  Created by Dusan Orescanin on 25/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
