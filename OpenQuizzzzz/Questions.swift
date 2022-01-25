d5f5269e679bdd71a7686fe85c8f1ed75a040c45
Dusan-O <dusan.orescanin@gmail.com>
2022-01-25 23:54:51 +0100

    Creating icons, LaunchScreen, and class Game in Game.swift

---
+++ OpenQuizzzzz/Questions.swift
@@ -0,0 +1,13 @@
+//
+//  Questions.swift
+//  OpenQuizzzzz
+//
+//  Created by Dusan Orescanin on 25/01/2022.
+//
+
+import Foundation
+
+struct Question {
+    var title = ""
+    var isCorrect = false
+}
