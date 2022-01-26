c3f1eef771809f8b5da7825039786a66d67933b3
Dusan-O <dusan.orescanin@gmail.com>
2022-01-26 13:43:01 +0100

    Connect Vue to Controller

--- OpenQuizzzzz/Controller/ViewController.swift
+++ OpenQuizzzzz/Controller/ViewController.swift
@@ -8,6 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionView: QuestionView!
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    private func startNewGame() {
+        activityIndicator.isHidden = false
+        newGameButton.isHidden = true
+        
+        questionView.title = "Loading..."
+        questionView.style = .standard
+        
+        scoreLabel.text = "0 / 10"
+    }
+    
+    @IBAction func didTapNewGameButton() {
+        startNewGame()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- OpenQuizzzzz/View/QuestionView.swift
+++ OpenQuizzzzz/View/QuestionView.swift
@@ -8,13 +8,38 @@
 import UIKit
 
 class QuestionView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    enum Style {
+        case correct, incorrect, standard
     }
-    */
-
+    
+    var style: Style = .standard {
+        didSet {
+            setStyle(style)
+        }
+    }
+    
+    private func setStyle(_ style: Style) {
+        switch style {
+        case .correct:
+            icon.image = UIImage(named: "Icon Correct")
+            icon.isHidden = false
+            backgroundColor = UIColor(red: 200.0/255.0, green: 236.0/255.0, blue: 160.0/255.0, alpha: 1) // Vert
+        case .incorrect:
+            icon.image = UIImage(named: "Icon Error")
+            icon.isHidden = false
+            backgroundColor = UIColor(red: 243.0/255.0, green: 135.0/255.0, blue: 148.0/255.0, alpha: 1) // Rouge
+        case .standard:
+            icon.isHidden = true
+            backgroundColor = UIColor(red: 191.0/255.0, green: 196.0/255.0, blue: 201.0/255.0, alpha: 1) // Gris
+        }
+    }
+    
+    var title = "" {
+        didSet {
+            label.text = title
+        }
+    }
+    
+    @IBOutlet private var label: UILabel!
+    @IBOutlet private var icon: UIImageView!
 }
