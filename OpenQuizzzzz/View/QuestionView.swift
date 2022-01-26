9a551775011dd2c6100fc2aa4bb6d8d5dbface33
Dusan-O <dusan.orescanin@gmail.com>
2022-01-26 02:43:13 +0100

    Creation Cocoa Touch Class in View

---
+++ OpenQuizzzzz/View/QuestionView.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionView.swift
+//  OpenQuizzzzz
+//
+//  Created by Dusan Orescanin on 26/01/2022.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
